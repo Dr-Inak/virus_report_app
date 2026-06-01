1c59f2b1541280b7c29ac72ccbf468e1fdddc06f
Dr-Inak <inak.mustapha@gmail.com>
2026-06-01 19:39:31 +0100

    finished the Nmber and mathematical operation exercise

---
+++ 02_num_math_ops.py
@@ -0,0 +1,29 @@
+int_01 = 55
+int_02 = 1.5
+int_03 = -5
+
+int_05 = 4
+int_06 = 10
+
+mod = int_06 % int_05
+floor_div = int_01 // int_02
+sum = int_01 - int_03
+expon = int_05 ** int_06
+
+
+float_func = float(int_01)
+int_func = int(int_02)
+round_func = round(int_02)
+absolute_func = abs(int_03)
+
+power_func = pow(int_03, int_02)
+
+print(type(int_02))
+print(type(sum))
+
+print('Summa:', sum)
+print('Modulo:', mod)
+print('Floor Division:', floor_div)
+print('Exponenetiation:', expon)
+print('power:', power_func)
+

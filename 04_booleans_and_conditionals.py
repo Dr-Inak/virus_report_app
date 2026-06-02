46859f8f046190f2ea783397802dcb5d1c5be58a
Dr-Inak <inak.mustapha@gmail.com>
2026-06-02 17:14:10 +0100

    built a movie ticket calculator

---
+++ 04_booleans_and_conditionals.py
@@ -0,0 +1,18 @@
+print(5 > 3)
+print(6 > 7)
+print(0 == 10)
+print(5 == 5)
+print(3 != 4)
+print(5 >= 4)
+print(5 <= 5)
+
+acolyte_sacrifice = False
+
+if acolyte_sacrifice :
+    print('Umbra Magna Est')
+elif 4 == 4:
+    print('Absentia Luci')
+else:
+    print("Tenebrae Vincunt")
+
+print(bool(acolyte_sacrifice))
